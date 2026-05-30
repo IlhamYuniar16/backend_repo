b4be9ed67df0e1cc830f4042b012a9f76db671fe
IlhamYuniar16 <ilhamyuniartioalfahrozi@gmail.com>
2026-05-30 14:38:53 +0700

    update backend

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['https://lynxxdevspace.vercel.app', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
